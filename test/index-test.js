9f61d5106e0a0c0df02c1b2f6c6ce49b87e6c04e
Seunggyu <seunggyul14@icloud.com>
2022-10-20 14:03:40 -0700

    commit tree 548ced0c5029953e2bf8e13fae3f64e53c1c68c5

--- test/index-test.js
+++ test/index-test.js
@@ -1,52 +1,29 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
-  })
-})
+function shout(string) {
+  return string.toUppercase();
+}
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello');
-  })
-})
+function whisper (string) {
+  return string.toLowercase();
+}
 
-describe('logShout(string)', function() {
-  it('takes a string argument and logs it in all caps using console.log()', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough();
+function logshout (string) {
+  console.log(sting.toHaveBeenCalledWith(`HELLO`))
+}
 
-    logShout('hello');
 
-    expect(spy).toHaveBeenCalledWith('HELLO');
+let lowercase = sayHiToHeadphonedRoommate.toLowercase
+let uppercase = sayHiToHeadphonedRoommate.toUppercase
+let message = "let's" + "have" + "dinner" + "together!"
+function sayHiToHeadphonedRoommate (){
+   if (sayHiToHeadphonedRoommate = lowercase)
+   return "i can't hear you"
 
-    console.log.restore();
-  })
-})
+   if (sayHiToHeadphonedRoommate = uppercase)
+   return "YES INDEED"
+   
+   else  (sayHiToHeadphonedRoommate = message)
+   return "i would love to!"
+}
 
-describe('logWhisper(string)', function() {
-  it('takes a string argument and logs it in all lowercase using console.log()', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough();
-
-    logWhisper('HELLO');
-
-    expect(spy).toHaveBeenCalledWith('hello');
-
-    console.log.restore();
-  })
-})
-
-describe('sayHiToHeadphonedRoommate(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
-  })
-
-  it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
-    expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
-      "I would love to!"
-    );
-  });
-})
+console.log(sayHiToHeadphonedRoommate)
